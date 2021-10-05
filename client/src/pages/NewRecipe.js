8967cdf92d310633a789080ce8ec7642d8b67cfe
joerenteria <joe@electronflight.com>
2021-10-05 14:28:21 -0500

    test

---
+++ client/src/pages/NewRecipe.js
@@ -0,0 +1,112 @@
+import { useState } from "react";
+import { useHistory } from "react-router";
+import styled from "styled-components";
+import ReactMarkdown from "react-markdown";
+import { Button, Error, FormField, Input, Label, Textarea } from "../styles";
+
+function NewRecipe({ user }) {
+  const [title, setTitle] = useState("Park name");
+  const [minutesToComplete, setMinutesToComplete] = useState("5");
+  const [instructions, setInstructions] = useState(`
+  The wild and untamed landscape of southern Utah contains some of the most 
+  rugged and inaccessible terrain in the country. Visitors to this area are 
+  awestruck by the enormity of the landscape – its vast size and colossal 
+  dimensions are overwhelming to the senses. 
+  `);
+  const [errors, setErrors] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const history = useHistory();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setIsLoading(true);
+    fetch("/recipes", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title,
+        instructions,
+        minutes_to_complete: minutesToComplete,
+      }),
+    }).then((r) => {
+      setIsLoading(false);
+      if (r.ok) {
+        history.push("/");
+      } else {
+        r.json().then((err) => setErrors(err.errors));
+      }
+    });
+  }
+
+  return (
+    <Wrapper>
+      <WrapperChild>
+        <h2>Create Entry</h2>
+        <form onSubmit={handleSubmit}>
+          <FormField>
+            <Label htmlFor="title">Title</Label>
+            <Input
+              type="text"
+              id="title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </FormField>
+          <FormField>
+            <Label htmlFor="minutesToComplete">Rating</Label>
+            <Input
+              type="number"
+              id="minutesToComplete"
+              value={minutesToComplete}
+              onChange={(e) => setMinutesToComplete(e.target.value)}
+            />
+          </FormField>
+          <FormField>
+            <Label htmlFor="instructions">Comments</Label>
+            <Textarea
+              id="instructions"
+              rows="10"
+              value={instructions}
+              onChange={(e) => setInstructions(e.target.value)}
+            />
+          </FormField>
+          <FormField>
+            <Button color="primary" type="submit">
+              {isLoading ? "Loading..." : "Submit Entry"}
+            </Button>
+          </FormField>
+          <FormField>
+            {errors.map((err) => (
+              <Error key={err}>{err}</Error>
+            ))}
+          </FormField>
+        </form>
+      </WrapperChild>
+      <WrapperChild>
+        <h1>{title}</h1>
+        <p>
+          <em>Rating: {minutesToComplete} minutes</em>
+          &nbsp;·&nbsp;
+          <cite>By {user.username}</cite>
+        </p>
+        <ReactMarkdown>{instructions}</ReactMarkdown>
+      </WrapperChild>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.section`
+  max-width: 1000px;
+  margin: 40px auto;
+  padding: 16px;
+  display: flex;
+  gap: 24px;
+`;
+
+const WrapperChild = styled.div`
+  flex: 1;
+`;
+
+export default NewRecipe;
---
+++ client/src/pages/RecipeList.js
@@ -0,0 +1,62 @@
+import { useEffect, useState } from "react";
+import ReactMarkdown from "react-markdown";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { Box, Button } from "../styles";
+
+function RecipeList({user}) {
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    fetch("/recipes")
+      .then((r) => r.json())
+      .then(setRecipes);
+  }, []);
+
+  function handleDelete(id) {
+    fetch(`/recipes/${id}`, {
+      method: "DELETE",
+    }).then(console.log("delete"));
+  }
+
+
+
+  return (
+    <Wrapper>
+      {recipes.length > 0 ? (
+        recipes.map((recipe) => (
+          <Recipe key={recipe.id}>
+            <Box>
+              <h2>{recipe.title}</h2>
+              <p>
+                <em>Rating: {recipe.minutesToComplete} minutes</em>
+                &nbsp;·&nbsp;
+                <cite>By {recipe.user.username}</cite>
+              </p>
+              <ReactMarkdown>{recipe.instructions}</ReactMarkdown>
+              <Button onClick={() => handleDelete(recipe.id)}>Delete</Button>
+            </Box>
+          </Recipe>
+        ))
+      ) : (
+        <>
+          <h2>None Found</h2>
+          <Button as={Link} to="/new">
+            Make new
+          </Button>
+        </>
+      )}
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.section`
+  max-width: 800px;
+  margin: 40px auto;
+`;
+
+const Recipe = styled.article`
+  margin-bottom: 24px;
+`;
+
+export default RecipeList;
